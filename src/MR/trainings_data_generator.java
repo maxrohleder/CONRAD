6903cf4834d9d7a40b690a30d65f3a9aa9ed76e6
rohleder <max.rohleder@fau.de>
2019-07-18 16:06:36 -0700

    added directory creation technique

--- src/MR/trainings_data_generator.java
+++ src/MR/trainings_data_generator.java
@@ -1,8 +1,12 @@
 package MR;
 
+import java.io.File;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.sql.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.concurrent.TimeUnit;
 
 import IceInternal.Time;
@@ -15,11 +19,11 @@ class data_generator{
 	public static String DATA_FOLDER = "I:\\bachelor\\data\\training\\simulation";
 	public static int X = 1240; // original resolution on the zeego detector in RSL is 1240x960 px
 	public static int Y = 960;
-	public static int NUMBER_OF_SAMPLES = 1;
+	public static int NUMBER_OF_SAMPLES = 10;
 	public static int NUMBER_OF_MATERIALS = 2;
 	
 	// system variables
-	private static Path root;
+	private static File root;
 	private static int serial_number = 0;
 	private static double scale = 0.08;
 	private static int[] resolution = new int[] {Y, X}; // 4th of zeego res
@@ -31,20 +35,31 @@ class data_generator{
 	
 	public static void main(String[] args) {
 		System.out.println("STARTING GENERATION OF " + target_number + " SAMPLES");
-		root = Paths.get(DATA_FOLDER);
+		root = new File(DATA_FOLDER);
 		
 		long starttime = Time.currentMonotonicTimeMillis();
 		double num_mins;
 		for (int i = 0; i < target_number; i++) {
+			File sample_dir = next_folder();
+			
 			// use methods in phantom_creator to generate a new random phantom 
-			AnalyticPhantom phantom = phantom_creator.create_new_phantom(300, 400);
+			//AnalyticPhantom phantom = phantom_creator.create_new_phantom(300, 400);
+			
 			// create and save projections of this phantom
-			generate_sample(phantom, false, true, true);
-			// increase serial number (for folder structure)
-			serial_number++;
+			//generate_sample(phantom, false, true, true);			
+			
 			num_mins = ((double)(Time.currentMonotonicTimeMillis() - starttime))/60000;
 			if(cnfg.DEBUG) System.out.println("generated sample " + i + "/" + target_number + 
-					" (took " + String.format("%.2f", num_mins) + "min total)");
+					" \n\t\t(" + String.format("%.2f", num_mins) + "min total, ~" + String.format("%.2f", num_mins/i) + "min per sample)");
+
+			// save projections 
+						save_sample(null, sample_dir);
+						try {
+							TimeUnit.SECONDS.sleep(5);
+						} catch (InterruptedException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
 		}
 	}
 	
@@ -53,15 +68,22 @@ class data_generator{
 	 * @param millies timestamp to create folder of
 	 * @return Path object containing a newly created directory according to current time
 	 */
-	private static Path next_folder(Date creation) {
-		String creation_formattet = creation.getMonth();
-		Path folder = Path.of(creation.toString());
-		return root.resolve(folder);
+	private static File next_folder() {
+		// packs current time in wrapper class
+		Calendar creation_time = Calendar.getInstance();
+		DateFormat dateFormat = new SimpleDateFormat("MMddHHmmss");
+		File folder = new File(root, dateFormat.format(creation_time.getTime()) + "_" + serial_number);
+		if(!folder.mkdirs()) {
+			System.err.println("[data_generator.next_folder]couldnt create folder:\n\t" + folder.toString());
+			System.exit(0);
+		}
+		serial_number++;
+		return folder;
 	}
 
 
-	private static void save_sample(Grid3D[] data, String foldername) {
-		System.out.println("sample saved in folder: " + foldername);
+	private static void save_sample(Grid3D data, File folder) {
+		System.out.println("sample saved in folder: \n\t" + folder.toString());
 		// TODO safe grids using CONRAD?
 	}
 
